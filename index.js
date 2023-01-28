cbf2f18af6a5adc68f6498d1f59474d18d51e0fe
Kalin Krastev <kkrastev@live.com>
2023-01-28 13:39:57 +0200

    Changed hello world message. To test CI/CD.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ buy: 'buddy' });
+  res.send({ message: 'Hello from a serverless containerized and continuously deployed Servey Maker!' });
 });
 
 const PORT = process.env.PORT || 5000;
