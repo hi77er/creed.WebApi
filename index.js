9b9dd17164e9ca0ba78d6abfdc5835a7a16c1fba
Kalin Krastev <kkrastev@live.com>
2023-01-29 12:29:14 +0200

    Version up.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.get('/', (req, res) => {
   res.send(
     {
-      version: "1.0.4",
+      version: "1.0.5",
       message: 'Hello from DEV branch of a serverless containerized and continuously deployed Servey Maker!'
     }
   );
