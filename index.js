60fe0459c313c6fb450a46408bcb627b7ca6a035
Kalin Krastev <kkrastev@live.com>
2023-01-29 11:39:04 +0200

    First dev changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ message: 'Hello from a serverless containerized and continuously deployed Servey Maker!' });
+  res.send({ message: 'Hello from DEV branch of a serverless containerized and continuously deployed Servey Maker!' });
 });
 
 const PORT = process.env.PORT || 5000;
