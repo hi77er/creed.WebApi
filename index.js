bf61124c4963d0f2c87619c7962e063a5bd3a31a
Kalin Krastev <kkrastev@live.com>
2023-02-03 10:51:53 +0200

    Installed dotenv package. Referenced env variables in code.

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
+require('dotenv').config();
 const express = require("express");
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const keys = require("./config/keys");
 const app = express();
 
 passport.use(
   new GoogleStrategy(
     {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
+      clientID: process.env.googleClientID,
+      clientSecret: process.env.googleClientSecret,
       callbackURL: '/oauth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
