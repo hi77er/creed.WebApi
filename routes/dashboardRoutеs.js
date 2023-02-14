38d46647c4f08312c8e89e29fe71c3894c0c7db2
Kalin Krastev <kkrastev@live.com>
2023-02-14 17:24:20 +0200

    Version upgrade.

--- routes/dashboardRoutеs.js
+++ routes/dashboardRoutеs.js
@@ -3,7 +3,7 @@ module.exports = (app) => {
   app.get('/', (req, res) => {
     res.send(
       {
-        version: "1.0.16",
+        version: "1.0.17",
         message: 'Hello from a serverless containerized and continuously deployed Servey Maker!',
         oauth: 'Google'
       }
